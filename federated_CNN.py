580c76f4e040372043a4918f606d29e71b124b8d
azure84 <azure84@naver.com>
2022-06-28 16:27:10 +0200

    first upload

---
+++ federated_CNN.py
@@ -0,0 +1,170 @@
+
+import os
+import random
+from tqdm import tqdm
+import numpy as np
+import torch, torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torchvision.datasets as dsets
+torch.backends.cudnn.benchmark=True
+
+
+class CNN(torch.nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        # First layer
+        # ImgIn shape=(?, 28, 28, 1)(batch size, width, height, channel)
+        #    Conv     -> (?, 28, 28, 32)
+        #    Pool     -> (?, 14, 14, 32)
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(), #activation function
+            torch.nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # 두번째층
+        # ImgIn shape=(?, 14, 14, 32)
+        #    Conv      ->(?, 14, 14, 64)
+        #    Pool      ->(?, 7, 7, 64)
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2))
+
+        # fully connected layer 7x7x64 inputs -> 10 outputs
+        self.fc = torch.nn.Linear(7 * 7 * 64, 10, bias=True)
+
+        # weight initialization
+        torch.nn.init.xavier_uniform_(self.fc.weight)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0),-1)   #Flatten, -1 means undetermined size(0) means first index
+        out = self.fc(out)
+        return out
+
+
+def client_update(client_model, optimizer, train_loader, epoch=5):
+    """
+    This function updates/trains client model on client data
+    """
+    model.train() # model for training
+    criterion = torch.nn.CrossEntropyLoss().to(device)  
+    for e in range(epoch):
+        for batch_idx, (data, target) in enumerate(train_loader): 
+            data, target = data.to(device) , target.to(device) 
+            optimizer.zero_grad()
+            output = client_model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+    return loss.item()
+
+
+def server_aggregate(global_model, client_models):
+    """
+    This function has aggregation method 'mean'
+    """
+    global_dict = global_model.state_dict() #current model parameter 
+
+    for k in global_dict.keys(): #iterate for whole model parameter
+    #stack the entire client model parameter and average the parameter value for global model
+    	global_dict[k] = torch.stack([client_models[i].state_dict()[k].float() for i in range(len(client_models))], 0).mean(0)
+    #update the global model parameter
+    global_model.load_state_dict(global_dict)
+
+    #update the client model parameter
+    for model in client_models:
+    	model.load_state_dict(global_model.state_dict())
+
+
+def test(global_model, test_loader):
+    """This function test the global model on test data and returns test loss and test accuracy """
+    model.eval() # model for testing
+    test_loss = 0
+    correct = 0
+    criterion = torch.nn.CrossEntropyLoss(reduction='sum').to(device)  
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device) , target.to(device) 
+            output = global_model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    acc = correct / len(test_loader.dataset)
+
+    return test_loss, acc
+
+
+
+##### Hyperparameters for federated learning #########
+num_clients = 20
+num_selected = 6
+num_rounds = 150
+epochs = 5
+batch_size = 25
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# random seed fix
+torch.manual_seed(777)
+
+# random seed fix for gpu
+if device == 'cuda':
+    torch.cuda.manual_seed_all(777)
+
+#learning_rate = 0.001	
+training_epochs = 15
+
+
+traindata = dsets.MNIST(root='MNIST_data/', # download path
+                          train=True, # True=training data download
+                          transform=transforms.ToTensor(), # transform to tensor
+                          download=True)
+
+# Dividing the training data into num_clients, with each client having equal number of images
+traindata_split = torch.utils.data.random_split(traindata, [int(traindata.data.shape[0] / num_clients) for _ in range(num_clients)])
+
+#loading the data for training 
+train_loader = [torch.utils.data.DataLoader(dataset=x, batch_size=batch_size, shuffle=True) for x in traindata_split]
+
+#loading the data for testing
+test_loader = torch.utils.data.DataLoader(
+        dsets.MNIST('MNIST_data/', train=False, transform=transforms.Compose([transforms.ToTensor()])
+        ), batch_size=batch_size, shuffle=True)
+
+
+global_model =  CNN().to(device)
+client_models = [ CNN().to(device) for _ in range(num_clients)]
+
+losses_train = []
+losses_test = []
+acc_train = []
+acc_test = []
+
+
+for model in client_models:
+    model.load_state_dict(global_model.state_dict()) ### initial synchronizing with global model 
+
+#optimizer
+opt = [optim.Adam(model.parameters(), lr=0.01) for model in client_models]
+
+
+for epoch in range(training_epochs):
+    loss = 0
+    client_idx = np.random.permutation(num_clients)[:num_selected] #random permutation for client selection
+    for i in tqdm(range(num_selected)):
+        loss += client_update(client_models[i], opt[i], train_loader[client_idx[i]], epoch=epochs)
+    losses_train.append(loss)
+
+    server_aggregate(global_model, client_models)
+    test_loss, acc = test(global_model, test_loader)
+    losses_test.append(test_loss)
+    acc_test.append(acc)
+    print('%d-th round' % epoch)
+    print('average train loss %0.3g | test loss %0.3g | test acc: %0.3f' % (loss / num_selected, test_loss, acc))
